3b9230a4bfd852a555656c57a19249e8f135661a
dave-nagib <davidnagib101@gmail.com>
2023-05-16 16:21:31 +0300

    Naming updates for MatrixHash

--- src/MatrixHash.java
+++ src/MatrixHash.java
@@ -1,6 +1,4 @@
 import static java.lang.Math.*;
-
-import java.util.ArrayList;
 import java.util.Random;
 public class MatrixHash {
 
@@ -12,9 +10,9 @@ public class MatrixHash {
     private int u;
 
     // Initialize h with a random b×u binary matrix.
-    public MatrixHash(int powerOf2, int maxBits) {
-        this.b = powerOf2;
-        this.u = maxBits;
+    public MatrixHash(int mapTo, int mapFrom) {
+        this.b = mapTo;
+        this.u = mapFrom;
         matrix = new int[b][u];
         Random random = new Random();
         // Randomly fill the matrix with 0's & 1's
