c3a1b4f4a6b58ae8b54475bf3699d71cc29cd859
Ehab Yasser <eng.ehabyasser@gmail.com>
2023-05-23 19:24:31 +0300

    fix main

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,9 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Random;
+import Dictionary.Dictionary;
+import Dictionary.IDictionary;
 
 public class Main {
-    private static final int MIN_STRING_LENGTH = 8; // Minimum length of each random string
-    private static final int MAX_STRING_LENGTH = 10; // Maximum length of each random string
-    private static final int NUM_STRINGS = 1000000; // Number of random strings to generate
-
     public static void main(String[] args) {
-        String fileName = "src/Tests/1M_Words.txt";
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            for (int i = 0; i < NUM_STRINGS; i++) {
-                String randomString = generateRandomString();
-                writer.write(randomString);
-                writer.newLine();
-            }
-            System.out.println("Random strings generated and written to file: " + fileName);
-        } catch (IOException e) {
-            System.err.println("An error occurred while writing to the file: " + e.getMessage());
-        }
-    }
-
-    private static String generateRandomString() {
-        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        Random random = new Random();
-        int length = random.nextInt(MAX_STRING_LENGTH - MIN_STRING_LENGTH + 1) + MIN_STRING_LENGTH;
-        StringBuilder sb = new StringBuilder(length);
-
-        for (int i = 0; i < length; i++) {
-            int index = random.nextInt(chars.length());
-            char randomChar = chars.charAt(index);
-            sb.append(randomChar);
-        }
-
-        return sb.toString();
+        IDictionary dictionary = new Dictionary();
+        dictionary.startProgram();
     }
 }
